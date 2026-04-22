7b04a952d999540f73bba3737b93865ed0d87eee
ColdKYZ <5022231153@student.its.ac.id>
2026-04-22 21:44:51 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+
+#define JUMLAH_SENSOR 16
+
+#define SEL_A 25
+#define SEL_B 26
+
+int sensorADC[JUMLAH_SENSOR];
+int sensorDigital[JUMLAH_SENSOR];
+int treshold = 500;
+
+int P, I, D, prevError, error;
+
+void bacaSensor() {
+  digitalWrite(SEL_A, 0); digitalWrite(SEL_B, 0);
+  delayMicroseconds(10);
+  sensorADC[14] = analogRead(32);
+  sensorADC[3]  = analogRead(33);
+  sensorADC[6]  = analogRead(34);
+  sensorADC[8]  = analogRead(35);
+
+  digitalWrite(SEL_A, 1); digitalWrite(SEL_B, 0);
+  delayMicroseconds(10);
+  sensorADC[12] = analogRead(32);
+  sensorADC[2]  = analogRead(33);
+  sensorADC[4]  = analogRead(34);
+  sensorADC[9]  = analogRead(35);
+
+  digitalWrite(SEL_A, 0); digitalWrite(SEL_B, 1);
+  delayMicroseconds(10);
+  sensorADC[13] = analogRead(32);
+  sensorADC[0]  = analogRead(33);
+  sensorADC[5]  = analogRead(34);
+  sensorADC[11] = analogRead(35);
+
+  digitalWrite(SEL_A, 1); digitalWrite(SEL_B, 1);
+  delayMicroseconds(10);
+  sensorADC[15] = analogRead(32);
+  sensorADC[1]  = analogRead(33);
+  sensorADC[7]  = analogRead(34);
+  sensorADC[10] = analogRead(35);
+
+  for(int i = 0; i < JUMLAH_SENSOR; i++) {
+    if (sensorADC[i] < 500) {
+      sensorDigital[i] = 0;
+    } else {
+      sensorDigital[i] = 1;
+    }
+
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(SEL_A, OUTPUT);
+  pinMode(SEL_B, OUTPUT);
+
+  analogReadResolution(12);       // 0–4095
+  analogSetAttenuation(ADC_11db); // range ~0–3.3V
+}
+
+void loop() {
+  bacaSensor();
+
+  // Tampilkan semua sensor
+  for (int i = 0; i < JUMLAH_SENSOR; i++) {
+    Serial.print(sensorDigital[i]);
+    Serial.print("\t"); // biar rapi (tab)
+  }
+  Serial.println();
+
+  delay(100);
+}
